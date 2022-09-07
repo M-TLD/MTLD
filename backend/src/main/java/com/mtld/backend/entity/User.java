1d4aabd52ed9b69d305cc11e34eb9c50436e8276
seongmin <psm7335@naver.com>
2022-09-07 17:41:39 +0900

    [#S07P22A106-15][BE] feat: User entity 추가

---
+++ backend/src/main/java/com/mtld/backend/entity/User.java
@@ -0,0 +1,33 @@
+package com.mtld.backend.entity;
+
+import com.mtld.backend.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * created by seongmin on 2022/09/07
+ */
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "USERS")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String oauthId;
+
+    private String platform;
+
+    private String name;
+
+    private String nickname;
+
+    private boolean isDeleted;
+
+}
