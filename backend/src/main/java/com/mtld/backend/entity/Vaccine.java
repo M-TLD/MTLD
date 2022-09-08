88fdde6a0ccd2a01cce6d274d1f6dbaa07d42478
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-09-08 11:48:43 +0900

    [#S07P22A106-60][BE] feat: Medicine, Vaccine enum으로 수정

--- backend/src/main/java/com/mtld/backend/entity/Vaccine.java
+++ backend/src/main/java/com/mtld/backend/entity/Vaccine.java
@@ -1,24 +1,8 @@
 package com.mtld.backend.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-/**
- * created by myeongseok on 2022/09/08
- */
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class Vaccine {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
+public enum Vaccine {
+    DHPPL,
+    COVID,
+    KENNELCOUGH,
+    RABIES
 }
