88ca50aed2131900cf766e8fe8a5152bc7bd0801
박성민 <psm7335@naver.com>
2022-09-13 17:03:23 +0900

    [#S07P22A106-19][BE] feat: 다이어리 request dto 생성

---
+++ backend/src/main/java/com/mtld/backend/dto/diary/WalkingRequestDto.java
@@ -0,0 +1,29 @@
+package com.mtld.backend.dto.diary;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+/**
+ * created by seongmin on 2022/09/08
+ * updated by seongmin on 2022/09/13
+ */
+
+@Getter
+@RequiredArgsConstructor
+public class WalkingRequestDto {
+
+    @NotNull(message = "산책한 날짜는 필수 입력값입니다.")
+    private LocalDateTime diaryDate;
+
+    @NotNull(message = "산책한 시간은 필수 입력값입니다.")
+    private Double walkingTime;
+
+    @NotNull(message = "산책한 거리는 필수 입력값입니다.")
+    private Double distance;
+
+    @NotNull(message = "산책일지 쓸 반려견은 필수 입력값입니다.")
+    private Long dogId;
+}
