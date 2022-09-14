09e213700484be97b563dc517c86d8c7c6fcad72
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-09-15 03:04:11 +0900

    [#S07P22A106-83][FE] feat: add responsive login page for desktop

--- frontend/mtld/src/pages/user/Login.jsx
+++ frontend/mtld/src/pages/user/Login.jsx
@@ -4,6 +4,7 @@ import kakaoLogin from 'assets/kakao-login.png';
 import mtldLogo from 'assets/logo.png';
 import pawStamp from 'assets/paw.png';
 import welcomeNote from 'assets/welcome-note.png';
+import { useMediaQuery } from 'react-responsive';
 
 const Container = styled.div`
   display: flex;
@@ -57,28 +58,61 @@ const KakaoBtn = styled.img`
   margin-bottom: 5%;
 `;
 
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  return isDesktop ? children : null;
+};
+
+const DesktopWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DesktopLogo = styled.img`
+  width: 50vw;
+  margin-bottom: 5%;
+`;
+
+const DesktopKakaoBtn = styled.img`
+  width: 50vw;
+`;
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+
 function Login() {
   return (
     <div className="login">
-      <Container>
-        <Logo src={mtldLogo} />
-        <Wrap>
-          <NoteImage>
-            <Note src={welcomeNote} />
-          </NoteImage>
-          <NoteText>
-            <p>멍! THE LAND</p>
-            <p>모든 강아지들의</p>
-            <p>원더랜드,</p>
-            <p>멍더랜드에 온 걸</p>
-            <p>환영하개</p>
-          </NoteText>
-          <Paw src={pawStamp} />
-        </Wrap>
-        <a href="http://localhost:8000/oauth2/authorization/kakao">
-          <KakaoBtn src={kakaoLogin} />
-        </a>
-      </Container>
+      <Desktop>
+        <DesktopWrap>
+          <DesktopLogo src={mtldLogo} />
+          <DesktopKakaoBtn src={kakaoLogin} />
+        </DesktopWrap>
+      </Desktop>
+      <Mobile>
+        <Container>
+          <Logo src={mtldLogo} />
+          <Wrap>
+            <NoteImage>
+              <Note src={welcomeNote} />
+            </NoteImage>
+            <NoteText>
+              <p>멍! THE LAND</p>
+              <p>모든 강아지들의</p>
+              <p>원더랜드,</p>
+              <p>멍더랜드에 온 걸</p>
+              <p>환영하개</p>
+            </NoteText>
+            <Paw src={pawStamp} />
+          </Wrap>
+          <a href="http://localhost:8000/oauth2/authorization/kakao">
+            <KakaoBtn src={kakaoLogin} />
+          </a>
+        </Container>
+      </Mobile>
     </div>
   );
 }
