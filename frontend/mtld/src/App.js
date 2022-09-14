b754fcfafde311f9df1148bef953a8497b94662d
신선영 <sunyeong0412@naver.com>
2022-09-14 10:28:13 +0900

    [#S07P22A106-24][FE] feat: delete features/counter relevants

--- frontend/mtld/src/App.js
+++ frontend/mtld/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      MTLD
-    </div>
-  );
+  return <div className="App">MTLD</div>;
 }
 
 export default App;
