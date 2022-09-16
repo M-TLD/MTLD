e9769c055cf9e81a32a399bca7fe61a4afb6d290
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-09-16 17:07:20 +0900

    [#S07P22A106-114][BE] feat: Dog 리포지터리 생성

--- backend/src/main/java/com/mtld/backend/repository/DogRepository.java
+++ backend/src/main/java/com/mtld/backend/repository/DogRepository.java
@@ -2,9 +2,15 @@ package com.mtld.backend.repository;
 
 import com.mtld.backend.entity.dog.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * created by seongmin on 2022/09/13
  */
 public interface DogRepository extends JpaRepository<Dog, Long> {
+
+    @Query("select d from Dog d where d.user.id = :uid")
+    List<Dog> findDogByUser(Long uid);
 }
