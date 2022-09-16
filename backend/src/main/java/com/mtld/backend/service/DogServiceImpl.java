b55164883ab835ec00bffd58e71508e55383c423
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-09-16 17:07:57 +0900

    [#S07P22A106-114][BE] feat: Dog 서비스 작성 (등록)

---
+++ backend/src/main/java/com/mtld/backend/service/DogServiceImpl.java
@@ -0,0 +1,46 @@
+package com.mtld.backend.service;
+
+import com.mtld.backend.dto.dog.DogRequestDto;
+import com.mtld.backend.entity.User;
+import com.mtld.backend.entity.dog.Breed;
+import com.mtld.backend.entity.dog.Dog;
+import com.mtld.backend.exception.BadRequestException;
+import com.mtld.backend.repository.BreedRepository;
+import com.mtld.backend.repository.DogRepository;
+import com.mtld.backend.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * created by MyeongSeok on 2022/09/14
+ * updated by MyeongSeok on 2022/09/16
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class DogServiceImpl implements DogService {
+    private final DogRepository dogRepository;
+    private final UserRepository userRepository;
+    private final BreedRepository breedRepository;
+
+
+    @Override
+    public void registerDog(Long uid, Long breedId, DogRequestDto dogRequestDto) {
+        User user = userRepository.findById(uid).orElseThrow(() -> new BadRequestException("유효하지 않은 사용자입니다."));
+        Breed breed = breedRepository.findById(breedId).orElseThrow(() -> new BadRequestException("유효하지 않은 품종입니다."));
+        Dog dog = Dog.builder()
+                .name(dogRequestDto.getName())
+                .birthdate(dogRequestDto.getBirthdate())
+                .gender(dogRequestDto.getGender())
+                .weight(dogRequestDto.getWeight())
+                .neuter(dogRequestDto.isNeuter())
+                .breed(breed)
+                .user(user).build();
+        dog.writeDisease(dogRequestDto.getDisease());
+
+        dogRepository.save(dog);
+    }
+}
