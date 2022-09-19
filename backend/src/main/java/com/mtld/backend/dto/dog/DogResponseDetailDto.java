d88beb9871f005afba9abca5f2f111ab48309ca6
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-09-19 20:55:07 +0900

    [#S07P22A106-122][BE]feat: DogDto 작성

---
+++ backend/src/main/java/com/mtld/backend/dto/dog/DogResponseDetailDto.java
@@ -0,0 +1,38 @@
+package com.mtld.backend.dto.dog;
+
+import com.mtld.backend.entity.dog.Dog;
+import com.mtld.backend.entity.dog.Gender;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * created by myeongseok on 2022/09/19
+ */
+@Data
+@Builder
+public class DogResponseDetailDto {
+
+    private String name;
+    private LocalDate birthdate;
+    private Gender gender;
+    private Double weight;
+    private boolean neuter;
+    private String disease;
+    private String breedName;
+    private String fileURL;
+
+    public static DogResponseDetailDto of(Dog dog) {
+        return DogResponseDetailDto.builder()
+                .name(dog.getName())
+                .birthdate(dog.getBirthdate())
+                .gender(dog.getGender())
+                .weight(dog.getWeight())
+                .neuter(dog.isNeuter())
+                .disease(dog.getDisease())
+                .breedName(dog.getBreed().getName())
+                .fileURL(dog.getFileURL())
+                .build();
+    }
+}
