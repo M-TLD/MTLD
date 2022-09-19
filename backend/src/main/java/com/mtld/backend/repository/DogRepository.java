6a3e66ddd0110f33e6d2ac2564fd34df9a45bdf8
seongmin <psm7335@naver.com>
2022-09-19 14:25:52 +0900

    [BE]feat: DogRepository에 메서드 추가

--- backend/src/main/java/com/mtld/backend/repository/DogRepository.java
+++ backend/src/main/java/com/mtld/backend/repository/DogRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * created by seongmin on 2022/09/13
+ * updated by seongmin on 2022/09/19
  */
 public interface DogRepository extends JpaRepository<Dog, Long> {
-
     List<Dog> findByUser(User user);
+    Optional<Dog> findByIdAndUser(Long id, User user);
 }
