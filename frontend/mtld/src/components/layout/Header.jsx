713be5caf084ac7f589f81744bc813abe3ddadfc
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-09-20 02:55:31 +0900

    [#S07P22A106-166][FE] feat: add logout

--- frontend/mtld/src/components/layout/Header.jsx
+++ frontend/mtld/src/components/layout/Header.jsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 import Logo from 'assets/mung.png';
 import Paw from 'assets/paw_yellow.png';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
 import Avatar from '@mui/material/Avatar';
+import { logout } from 'app/user';
 
 const StyledHeader = styled.header`
   .Contents {
@@ -72,6 +74,13 @@ const StyledHeader = styled.header`
     font-weight: bold;
   }
 
+  .logout {
+    color: #5c5c5c;
+    font-family: 'UhBeeStrawberry';
+    font-size: 19px;
+    font-weight: bold;
+  }
+
   .name {
     font-size: 20px;
   }
@@ -84,6 +93,7 @@ const LogoImage = styled.img`
 
 function Header() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const locationNow = useLocation();
   if (locationNow.pathname === '/login') return null;
   return (
@@ -98,22 +108,34 @@ function Header() {
           </div>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            <div className="menuDiv">
-              <div className="userDiv">
-                <Avatar sx={{ height: '7vh', width: '7vh' }} />
-                <p className="name">보비</p>
+          <div className="menuDiv">
+            <div className="userDiv">
+              <Avatar sx={{ height: '7vh', width: '7vh' }} />
+              <p className="name">보비</p>
+            </div>
+            <div className="userInfoDiv">
+              <a href="/mypage">마이페이지</a>
+              <div
+                className="logout"
+                onClick={() => {
+                  dispatch(logout());
+                  window.alert('성공적으로 로그아웃 되었습니다');
+                  navigate('/login');
+                }}
+                onKeyDown={() => {
+                  dispatch(logout());
+                }}
+                role="button"
+                tabIndex={0}
+              >
+                로그아웃
               </div>
-              <div className="userInfoDiv">
-                <a href="/mypage">마이페이지</a>
-                <a href="/logout">로그아웃</a>
-                <div className="right" role="button" onClick={() => navigate(-1)} onKeyDown={console.log('back!')} tabIndex={0}>
-                  <img className="paw" src={Paw} alt="paw" />
-                  <div className="goback">뒤로가기</div>
-                </div>
+              <div className="right" role="button" onClick={() => navigate(-1)} onKeyDown={console.log('back!')} tabIndex={0}>
+                <img className="paw" src={Paw} alt="paw" />
+                <div className="goback">뒤로가기</div>
               </div>
             </div>
-          </Typography>
+          </div>
         </AccordionDetails>
       </Accordion>
     </StyledHeader>
