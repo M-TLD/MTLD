52b1eb422236e9981fd738e61d4e708182f9d6d7
MinjinChae <eominjin25@gmail.com>
2022-09-19 17:04:28 +0900

    [S07P22A106-165][FE] feat: get the geographical position(current location) of a user & using geolocation api

--- frontend/mtld/src/components/common/KakaoMap.jsx
+++ frontend/mtld/src/components/common/KakaoMap.jsx
@@ -1,24 +1,51 @@
-/* global kaka */
-import React, { useEffect, useRef } from 'react';
-
-const options = {
-  // 지도를 생성할 때 필요한 기본 옵션
-  center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표.
-  level: 3, // 지도의 레벨(확대, 축소 정도)
-};
+/* global kakao */
+import React, { useRef, useEffect, useState } from 'react';
 
 function KakaoMap() {
-  const container = useRef(null); // 지도를 담을 영역의 DOM 레퍼런스
+  const [map, setMap] = useState(null);
+  let lat;
+  let long;
   useEffect(() => {
-    const kmap = new window.kakao.maps.Map(container.current, options); // 지도 생성 및 객체 리턴
+    // geoloaction으로 사용할 수 있다면
+    if (navigator.geolocation) {
+    // GeoLocation을 이용해 접속 위치 얻어오기
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          lat = position.coords.latitude; // 위도
+          long = position.coords.longitude; // 경도
+          console.log(lat, long);
+
+          const options = {
+            // 지도를 생성할 때 필요한 기본 옵션
+            center: new window.kakao.maps.LatLng(lat, long),
+            // 지도의 중심좌표: 제주도 카카오 -> 현위치로 바꿔야함
+            level: 3, // 지도의 레벨(확대, 축소 정도)
+          };
+          const container = document.getElementById('map');
+          const kakaoMap = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴 얘 뭔데
+          setMap(kakaoMap);
+        },
+        // (error) => {
+        //   lat = false;
+        //   long = false;
+
+        //   throw new Error(error);
+        // },
+
+      );
+    } else {
+      lat = false;
+      long = false;
+    }
+
     return () => {};
   }, []);
 
   return (
     <div
-      id="kmap"
+      id="map"
       style={{ width: '500px', height: '500px' }}
-      ref={container}
+
     />
   );
 }
