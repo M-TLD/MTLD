b0f65dffde80014620e7995511288b88383c0f29
MinjinChae <eominjin25@gmail.com>
2022-09-20 17:26:27 +0900

    [S07P22A106-174][FE] feat: add button for  current position

--- frontend/mtld/src/components/common/KakaoMap.jsx
+++ frontend/mtld/src/components/common/KakaoMap.jsx
@@ -1,13 +1,13 @@
 /* global kakao */
 import React, { useRef, useEffect, useState } from 'react';
 
-function KakaoMap({ searchPlace }) {
+function KakaoMap({ searchPlace, flag }) {
   const [map, setMap] = useState(null);
-
   let lat;
   let long;
-
   useEffect(() => {
+    console.log(searchPlace);
+    console.log(flag);
     // geoloaction으로 사용할 수 있다면
     if (navigator.geolocation) {
     // GeoLocation을 이용해 접속 위치 얻어오기
@@ -16,36 +16,38 @@ function KakaoMap({ searchPlace }) {
           lat = position.coords.latitude; // 위도
           long = position.coords.longitude; // 경도
           console.log(lat, long);
-
-          const options = {
+          // 검색하지 않았거나 flag가 true === 현재위치설정 버튼을 눌렀다면
+          if (searchPlace === '' || flag === true) {
+            const options = {
             // 지도를 생성할 때 필요한 기본 옵션
-            center: new window.kakao.maps.LatLng(lat, long),
-            // 지도의 중심좌표: 제주도 카카오 -> 현위치로 바꿔야함
-            level: 3, // 지도의 레벨(확대, 축소 정도)
-          };
-          const container = document.getElementById('map');
-          const kakaoMap = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴 얘 뭔데
-          setMap(kakaoMap);
-          const ps = new kakao.maps.services.Places(); // 장소 검색 객체 생성
-          ps.keywordSearch(searchPlace, placeSearchCB);
-          function placeSearchCB(data, status, _pagination) {
-            if (status === kakao.maps.services.Status.OK) {
-              const bounds = new kakao.maps.LatLngBounds();
-
-              for (let i = 0; i < data.length; i += 1) {
-                displayMarker(data[i]);
-                // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-                // LatLngBounds 객체에 좌표를 추가
-                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+              center: new window.kakao.maps.LatLng(lat, long),
+              // 지도의 중심좌표: 제주도 카카오 -> 현위치로 바꿔야함
+              level: 3, // 지도의 레벨(확대, 축소 정도)
+            };
+            const container = document.getElementById('map');
+            const kakaoMap = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴 얘 뭔데
+            setMap(kakaoMap);
+          } else {
+            const ps = new kakao.maps.services.Places(); // 장소 검색 객체 생성
+            ps.keywordSearch(searchPlace, placeSearchCB);
+            function placeSearchCB(data, status, _pagination) {
+              if (status === kakao.maps.services.Status.OK) {
+                const bounds = new kakao.maps.LatLngBounds();
+                for (let i = 0; i < data.length; i += 1) {
+                  displayMarker(data[i]);
+                  // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                  // LatLngBounds 객체에 좌표를 추가
+                  bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }
+                map.setBounds(bounds); // 검색된 장소 위치를 기준으로 지도 범위를 재설정
               }
-              map.setBounds(bounds); // 검색된 장소 위치를 기준으로 지도 범위를 재설정
             }
-          }
-          function displayMarker(place) {
-            const marker = new kakao.maps.Marker({
-              map,
-              position: new kakao.maps.LatLng(place.y, place.x),
-            });
+            function displayMarker(place) {
+              const marker = new kakao.maps.Marker({
+                map,
+                position: new kakao.maps.LatLng(place.y, place.x),
+              });
+            }
           }
         },
         // (error) => {
@@ -62,14 +64,15 @@ function KakaoMap({ searchPlace }) {
     }
 
     return () => {};
-  }, [searchPlace]);
+  }, [searchPlace, flag]);
 
   return (
-    <div
-      id="map"
-      style={{ width: '500px', height: '500px' }}
-
-    />
+    <div>
+      <div
+        id="map"
+        style={{ width: '500px', height: '500px' }}
+      />
+    </div>
   );
 }
 
--- frontend/mtld/src/pages/location/Hospital.jsx
+++ frontend/mtld/src/pages/location/Hospital.jsx
@@ -4,7 +4,7 @@ import KakaoMap from 'components/common/KakaoMap';
 function Hospital() {
   const [InputText, setInputText] = useState('');
   const [Place, setPlace] = useState('');
-
+  const [flag, setFlag] = useState(false);
   const onChange = (e) => {
     setInputText(e.target.value);
   };
@@ -13,6 +13,11 @@ function Hospital() {
     e.preventDefault();
     setPlace(InputText);
     setInputText('');
+    setFlag(false);
+  };
+
+  const handleChange = () => {
+    setFlag(true);
   };
 
   return (
@@ -21,7 +26,8 @@ function Hospital() {
         <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
         <button type="submit">검색</button>
       </form>
-      <KakaoMap searchPlace={Place} />
+      <KakaoMap searchPlace={Place} flag={flag} />
+      <button type="button" onClick={handleChange}>현재위치중심</button>
     </div>
   );
 }
