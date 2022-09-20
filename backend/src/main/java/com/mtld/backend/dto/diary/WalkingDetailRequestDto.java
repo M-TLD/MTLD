70a15a224496b5715df748db95099d40e520ea18
seongmin <psm7335@naver.com>
2022-09-20 17:31:09 +0900

    [BE] feat: WalkingDetailResponseDto diaryDate type String으로 변경

--- backend/src/main/java/com/mtld/backend/dto/diary/WalkingDetailRequestDto.java
+++ backend/src/main/java/com/mtld/backend/dto/diary/WalkingDetailRequestDto.java
@@ -8,10 +8,11 @@ import javax.validation.constraints.NotNull;
 
 /**
  * created by seongmin on 2022/09/19
+ * updated by seongmin on 2022/09/20
  */
 @Getter
 @AllArgsConstructor
-public class DiaryDetailRequestDto {
+public class WalkingDetailRequestDto {
     @NotNull(message = "강아지 아이디는 필수 입력값 입니다.")
     private Long dogId;
     @NotBlank(message = "날짜는 필수 입력값 입니다.")
