66ea1b4b384f8d077cfb0dca274fab2a582b559d
seongmin <psm7335@naver.com>
2022-09-20 16:39:16 +0900

    [BE]feat: String 날짜를 LocalDate type으로 변환하는 util 메서드 추가

--- backend/src/main/java/com/mtld/backend/dto/diary/WalkingDetailRequestDto.java
+++ backend/src/main/java/com/mtld/backend/dto/diary/WalkingDetailRequestDto.java
@@ -3,17 +3,17 @@ package com.mtld.backend.dto.diary;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 
 /**
  * created by seongmin on 2022/09/19
  */
 @Getter
 @AllArgsConstructor
-public class WalkingDetailRequestDto {
+public class DiaryDetailRequestDto {
     @NotNull(message = "강아지 아이디는 필수 입력값 입니다.")
     private Long dogId;
-    @NotNull(message = "날짜는 필수 입력값 입니다.")
-    private LocalDate diaryDate;
+    @NotBlank(message = "날짜는 필수 입력값 입니다.")
+    private String diaryDate;
 }
