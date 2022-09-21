8b2d05432e28e27e6191e8b776b3b3d63af49ee9
MinjinChae <eominjin25@gmail.com>
2022-09-21 11:05:47 +0900

    [S07P22A106-168][FE] feat: add recommend hospital

--- frontend/mtld/src/components/common/KakaoMap.jsx
+++ frontend/mtld/src/components/common/KakaoMap.jsx
@@ -17,7 +17,7 @@ function KakaoMap({ searchPlace, flag }) {
           long = position.coords.longitude; // 경도
           console.log(lat, long);
           // 검색하지 않았을 때
-          if (searchPlace === '') {
+          if (searchPlace === '' && flag === false) {
             const options = {
             // 지도를 생성할 때 필요한 기본 옵션
               center: new window.kakao.maps.LatLng(lat, long),
@@ -29,6 +29,35 @@ function KakaoMap({ searchPlace, flag }) {
             setMap(kakaoMap);
           } else if (flag === true) {
             console.log('버튼 눌렀을 때');
+            const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+            const ps = new kakao.maps.services.Places(); // 장소 검색 객체 생성
+            ps.keywordSearch('동물병원', placeSearchCB, {
+              location: new window.kakao.maps.LatLng(lat, long),
+            });
+            function placeSearchCB(data, status, _pagination) {
+              if (status === kakao.maps.services.Status.OK) {
+                const bounds = new kakao.maps.LatLngBounds();
+                for (let i = 0; i < data.length; i += 1) {
+                  displayMarker(data[i]);
+                  // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                  // LatLngBounds 객체에 좌표를 추가
+                  bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }
+                map.setBounds(bounds); // 검색된 장소 위치를 기준으로 지도 범위를 재설정
+              }
+            }
+            function displayMarker(place) {
+              const marker = new kakao.maps.Marker({
+                map,
+                position: new kakao.maps.LatLng(place.y, place.x),
+              });
+              // 마커에 클릭이벤트를 등록
+              kakao.maps.event.addListener(marker, 'click', () => {
+                // 장소명이 인포윈도우에 표출됨
+                infowindow.setContent(`<div style="padding:5px;font-size:12px;">${place.place_name}</div>`);
+                infowindow.open(map, marker);
+              });
+            }
           } else {
             const infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
             const ps = new kakao.maps.services.Places(); // 장소 검색 객체 생성
