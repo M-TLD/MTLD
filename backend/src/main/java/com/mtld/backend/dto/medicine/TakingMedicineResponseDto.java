4b445980b2eb7bcb71cb8a1aeea4254e7304bc6d
명석 <37326982+audtjr9514@users.noreply.github.com>
2022-09-23 17:29:33 +0900

    [#S07P22A106-182][BE]feat: Medicine Dto 작성

---
+++ backend/src/main/java/com/mtld/backend/dto/medicine/TakingMedicineResponseDto.java
@@ -0,0 +1,30 @@
+package com.mtld.backend.dto.medicine;
+
+import com.mtld.backend.entity.medicine.Medicine;
+import com.mtld.backend.entity.medicine.TakingMedicine;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * created by myeongseok on 2022/09/21
+ */
+@Data
+@Builder
+public class TakingMedicineResponseDto {
+
+    private Long id;
+    private Long dogId;
+    private Long medicineId;
+    private LocalDate expectDate;
+
+    public static TakingMedicineResponseDto of(TakingMedicine takingMedicine) {
+        return TakingMedicineResponseDto.builder()
+                .id(takingMedicine.getId())
+                .dogId(takingMedicine.getDog().getId())
+                .medicineId(takingMedicine.getMedicine().getId())
+                .expectDate(takingMedicine.getExpectDate())
+                .build();
+    }
+}
