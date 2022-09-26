fc9ab9eb3253107d78ded5524e85c6477964ae79
seongmin <psm7335@naver.com>
2022-09-26 16:46:51 +0900

    [#][BE] bugFix: 생성자 에러 수정

--- backend/src/main/java/com/mtld/backend/dto/diary/record/RecordRequestDto.java
+++ backend/src/main/java/com/mtld/backend/dto/diary/record/RecordRequestDto.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
  */
 
 @Getter
-@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
--- backend/src/main/java/com/mtld/backend/dto/diary/walking/WalkingRequestDto.java
+++ backend/src/main/java/com/mtld/backend/dto/diary/walking/WalkingRequestDto.java
@@ -16,7 +16,6 @@ import java.time.LocalDateTime;
  */
 
 @Getter
-@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class WalkingRequestDto {
