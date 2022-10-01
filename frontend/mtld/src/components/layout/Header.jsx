8938283caba9175331d8766fb5a793d1edd1b88b
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-10-02 06:26:46 +0900

    [#S07P22A106-173][FE] feat: add dog info to navbar

--- frontend/mtld/src/components/layout/Header.jsx
+++ frontend/mtld/src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Logo from 'assets/mung.png';
 import Paw from 'assets/paw_yellow.png';
@@ -10,6 +10,8 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import MenuIcon from '@mui/icons-material/Menu';
 import Avatar from '@mui/material/Avatar';
 import { logout } from 'app/user';
+import { fetchPuppyInfo, puppySelector } from 'app/puppy';
+import Spinner from 'components/common/Spinner';
 
 const StyledHeader = styled.header`
   .Contents {
@@ -77,24 +79,25 @@ function Header() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const locationNow = useLocation();
+  const puppy = useSelector(puppySelector);
 
   const [expand, setExpand] = React.useState(false);
   const toggleAccordion = () => {
     setExpand((prev) => !prev);
   };
 
+  useEffect(() => {
+    dispatch(fetchPuppyInfo());
+  }, []);
+
   if (locationNow.pathname === '/login') return null;
+  if (!puppy.puppyInfo) {
+    return <div>loading...</div>;
+  }
   return (
     <StyledHeader>
-      <Accordion
-        expanded={expand}
-        sx={{ bgcolor: '#ffeeb1', zIndex: '100', width: '100vw', position: 'fixed' }}
-      >
-        <AccordionSummary
-          expandIcon={<MenuIcon onClick={toggleAccordion} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
+      <Accordion expanded={expand} sx={{ bgcolor: '#ffeeb1', zIndex: '100', width: '100vw', position: 'fixed' }}>
+        <AccordionSummary expandIcon={<MenuIcon onClick={toggleAccordion} />} aria-controls="panel1a-content" id="panel1a-header">
           <div className="navBar">
             <Link className="Link" to="/" style={{ zIndex: '100' }}>
               <LogoImage src={Logo} />
@@ -104,8 +107,8 @@ function Header() {
         <AccordionDetails>
           <div className="menuDiv">
             <div className="userDiv">
-              <Avatar sx={{ height: '7vh', width: '7vh' }} />
-              <p className="name">보비</p>
+              <Avatar src={puppy.puppyInfo[0].fileURL} sx={{ height: '7vh', width: '7vh' }} />
+              <p className="name">{puppy.puppyInfo[0].name}</p>
             </div>
             <div className="userInfoDiv">
               <a href="/mypage">마이페이지</a>
