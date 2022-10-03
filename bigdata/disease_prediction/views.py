b1f3995e385857046cb7aa7d713c06a848a2554e
연승용 <yeonstar1@gmail.com>
2022-10-03 17:05:42 +0900

    [#][BD] feat: 질병예측

--- bigdata/disease_prediction/views.py
+++ bigdata/disease_prediction/views.py
@@ -11,33 +11,35 @@ from collections import OrderedDict
 @api_view(['GET','POST'])
 def disease_predict(request):
    if request.method=='POST':
-      return HttpResponse(request.POST['submitRecord'])
+      data=json.loads(request.body.decode('utf-8'))
+      print(' '.join(data))
+      condition=' '.join(data)
+
       df = pd.read_csv('disease_prediction/disease_data/disease.csv',engine='python', encoding='cp949')
 
       condition_split=condition.split()
       current_condition=pd.DataFrame(data=[['9999', condition]], columns=['num','condition'])
       df=df.append(current_condition)
-      df.set_index('num')
+      # df.set_index('num')
 
       df_condition=df['condition'].fillna('').values
       index=df['num']
       tf_idf_model=TfidfVectorizer().fit(df_condition)
       word_id_list=sorted(tf_idf_model.vocabulary_.items(),key=lambda x: x[1], reverse=False)
       word_list=[x[0] for x in word_id_list]
-      tf_idf_df=pd.DataFrame(tf_idf_model.transform(df_condition).toarray(), columns=word_list, index=index)
+      tf_idf_df=pd.DataFrame(tf_idf_model.transform(df_condition).toarray(), columns=word_list)
       cos_sim_df=pd.DataFrame(cosine_similarity(tf_idf_df,tf_idf_df),columns=index,index=index)
 
-      word_df=tf_idf_df.max(axis=0)
-      for c in condition_split:
-         word_df[c]=1
+      # word_df=tf_idf_df.max(axis=0)
+      # for c in condition_split:
+      #    word_df[c]=1
       # word_df.sort_values().index[0:5]
 
       result=cos_sim_df.loc['9999']
       result['9999']=0
       result=result.sort_values(ascending=False)
-      data=df.loc[result.index[0:3]-1].reset_index()
-      return HttpResponse(data[['disease_name','define']].to_json(orient='records'))
-      # return HttpResponse(df.loc[result.index[0:5]-1].to_json(orient='records'))
+      data=df.loc[result.index[0:5]-1]
+      return HttpResponse(df.loc[result.index[0:5]-1].to_json(orient='records'))
 
    elif request.method=='GET':
       condition=pd.read_csv('disease_prediction/disease_data/condition.csv',engine='python', encoding='cp949')
