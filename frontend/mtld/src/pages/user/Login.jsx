ab6468f7a8edbfa9ae76d262a3384084fb7c0fd9
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-10-06 10:04:54 +0900

    [#][FE] chore: change kakao redirect url

--- frontend/mtld/src/pages/user/Login.jsx
+++ frontend/mtld/src/pages/user/Login.jsx
@@ -83,8 +83,8 @@ const Mobile = ({ children }) => {
 };
 
 function Login() {
-  const KAKAO_CLIENT_ID = 'b16fa4858c60ea9d4f042ea3b51165db';
-  const REDIRECT_URI = 'http://localhost:3000/login/oauth2/kakao';
+  const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return (
